68e4fd3f76fca0581355a873e9e4f0f7f2a8677a
cfg40 <cristhiansito40@gmail.com>
2025-08-30 18:26:29 -0500

    Subiendo archivos al repositorio

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Esta variable contiene los nombres de los amigos
+let amigos = [];
+let inputNombreAmigo;
+
+function agregarAmigo() {
+    inputNombreAmigo = document.getElementById("amigo").value;
+
+    if (inputNombreAmigo === "") {
+        alert ("Por favor, inserte un nombre ");
+    } else {
+        mostrarLista()
+        document.getElementById("amigo").value = "";  
+    }
+}
+
+function mostrarLista (){       
+    let lista = document.getElementById("listaAmigos")
+    lista.innerHTML="";
+    amigos.push(inputNombreAmigo);
+
+    for (let i=0; i < amigos.length; i++){
+        let elementoLista= document.createElement("li");
+        elementoLista.innerHTML=amigos[i];
+        lista.appendChild(elementoLista);
+     }
+}
+
+function sortearAmigo() {
+    let lista = document.getElementById("listaAmigos")
+    let amigoSorteado = Math.floor(Math.random() * amigos.length);
+    let mostrarResultado = document.getElementById("resultado");
+    mostrarResultado.innerHTML = (`El amigo secreto sorteado es: ${amigos[amigoSorteado]}`);
+    lista.innerHTML="";
+    amigos=[];
+}
